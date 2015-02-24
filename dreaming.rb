1efd2effa2400330a54d6a053f216677d8965b81
Matthew Bunday <zenceph@gmail.com>
2015-02-24 09:05:08 -0500

    Add dreaming file.

---
+++ dreaming.rb
@@ -0,0 +1,18 @@
+def a
+  puts "I'm entering the first dream"
+  b
+  puts "Woke up from first dream"
+end
+
+def b
+   puts "entering the second dream"
+   c
+   puts "Woke up from second dream"
+end
+
+def c
+   puts "entering the third dream"
+   puts "Woke up from third dream"
+end
+
+a
